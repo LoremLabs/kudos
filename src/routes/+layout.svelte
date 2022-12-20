795d0a2731c34e212a8ce359ff33e024d913e0fe
Matt Mankins <mankins@gmail.com>
2022-12-20 18:11:22 +0100

    feature: window title for mac

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,36 @@
 
   import { listen } from "@tauri-apps/api/event";
   import { goto } from "$app/navigation";
+  import { dev } from "$app/environment";
 
   import { onMount } from "svelte";
+  import { appWindow } from "@tauri-apps/api/window";
+
+  const disableContextMenu = () => {
+    document.addEventListener(
+      "contextmenu",
+      (e) => {
+        e.preventDefault();
+        return false;
+      },
+      { capture: true }
+    );
+
+    document.addEventListener(
+      "selectstart",
+      (e) => {
+        e.preventDefault();
+        return false;
+      },
+      { capture: true }
+    );
+  };
 
   onMount(() => {
+    if (!dev) {
+      disableContextMenu();
+    }
+
     listen("show-preferences", (event) => {
       // navigate to page 2
       // https://kit.svelte.dev/docs#routing-pages
@@ -19,6 +45,11 @@
   });
 </script>
 
-<main class="w-full bg-slate-100 min-h-screen p-2">
+
+<div data-tauri-drag-region class="fixed top-0 left-0 justify-center flex bg-slate-100 h-10 w-full pt-2 italic select-none cursor-default">
+Setler
+</div>        
+
+<main class="w-full bg-slate-100 min-h-screen p-2 mt-8">
   <slot />
 </main>
