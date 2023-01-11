7a213089912273b231e833f0e54fdee3a0ce6e6e
Matt Mankins <mankins@gmail.com>
2023-01-11 12:49:55 +0100

    feature[enola]: scaffold sveltekit

---
+++ packages/enola/src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+	import '$lib/styles/tailwind.css';
+</script>
+
+<slot />
---
+++ packages/enola/src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<p class="text-xl">This is a test of the emergency broadcast system.</p>
+<p class="text-xl bg-red-100">This is only a test.</p>
