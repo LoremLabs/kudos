e9ec248c947f3446f23db23cb08b9fb5e90290e3
Matt Mankins <mankins@gmail.com>
2023-01-11 14:59:11 +0100

    feature[enola]: remove homepage

--- packages/enola/src/routes/+page.svelte
+++
@@ -1,3 +0,0 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-<p class="text-xl">This is a test of the emergency broadcast system.</p>
-<p class="text-xl bg-red-100">This is only a test.</p>
