053dd869f59693380e2ca5353f62e5900cf39706
Matt Mankins <mankins@gmail.com>
2023-01-18 14:38:40 +0100

    feature(enola): rough in gql

--- packages/enola/src/lib/gql/index.js
+++ packages/enola/src/lib/gql/index.js
@@ -1,19 +1,33 @@
+import { resolvers as scalarResolvers, typeDefs as scalarTypeDefs } from 'graphql-scalars';
+
 import Base from './schema/base.gql';
-import DateTimeSchema from './schema/datetime.gql';
+import CryptographySchema from './schema/cryptography.gql';
 import KudosSchema from './schema/kudos.gql';
 import MoneySchema from './schema/money.gql';
 import Resolvers from './resolvers';
+import SortSchema from './schema/sort.gql';
 import TempSchema from './schema/temp.gql';
-import TypesSchema from './schema/types.gql';
 import UserSchema from './schema/user.gql';
+import VerifiableCredentials from './schema/verifiable-credentials.gql';
 
 // The GraphQL schema in string form
-const typeDefs = [DateTimeSchema, UserSchema, TypesSchema, MoneySchema, KudosSchema, TempSchema];
+const typeDefs = [
+	...scalarTypeDefs,
+	UserSchema,
+	MoneySchema,
+	KudosSchema,
+	TempSchema,
+	SortSchema,
+	CryptographySchema,
+	VerifiableCredentials,
+	Base
+];
 
 // A map of functions which return data for the schema.
 const resolvers = {
+    ...scalarResolvers,
 	Query: {
-        ...Resolvers,
+		...Resolvers
 	}
 };
 
