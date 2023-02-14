bda5c763ae68fd6ed89434f127687425a23a88a1
Matt Mankins <mankins@gmail.com>
2023-02-14 19:35:14 +0100

    feature: auto-update time

--- src/lib/components/Ago.svelte
+++ src/lib/components/Ago.svelte
@@ -1,16 +1,29 @@
 <script>
   import { format } from 'timeago.js';
 
-  import { onMount } from 'svelte';
+  import { onMount, onDestroy } from 'svelte';
   export let at = new Date() + '';
   let loaded = false;
+  let formatted = '';
+  let updater;
   onMount(() => {
     loaded = true;
+    formatted = format(new Date(at));
+
+    // update every 20 seconds
+    updater = setInterval(() => {
+      formatted = format(new Date(at));
+    }, 20000);
+  });
+
+  // on destroy, clear the interval
+  onDestroy(() => {
+    clearInterval(updater);
   });
 </script>
 
 {#if loaded}
-  <time datetime={at} title={at}>{format(new Date(at))}</time>
+  <time datetime={at} title={at}>{formatted}</time>
 {:else}
   -
 {/if}
