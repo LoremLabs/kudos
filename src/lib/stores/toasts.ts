d4ad4e59388e1b5c246ff95ea2d7489687738abb
Matt Mankins <mankins@gmail.com>
2023-02-20 19:46:57 +0100

    feature: duration to toasts

--- src/lib/stores/toasts.ts
+++ src/lib/stores/toasts.ts
@@ -5,6 +5,7 @@ type Toast = {
   msg: string;
   id?: string;
   type: 'success' | 'error' | 'info' | 'warn' | 'neutral';
+  duration?: number;
 };
 
 export const toasts = writable<Toast[]>([]);
@@ -12,7 +13,8 @@ export const toasts = writable<Toast[]>([]);
 export function addToast(toast: Toast) {
   toast.id = shortId();
   toasts.update((state) => [toast, ...state]);
-  setTimeout(removeToast, 30000);
+  toast.duration = toast.duration || 30000;
+  setTimeout(removeToast, toast.duration);
 }
 
 function removeToast() {
