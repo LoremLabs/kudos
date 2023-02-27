ef9f841f39d1420454deb77e88c79c79c64f67f2
Matt Mankins <mankins@gmail.com>
2023-02-27 10:31:17 +0100

    feature: settle for fame, p1

--- src/lib/components/ModalStartFameDistribution.svelte
+++ src/lib/components/ModalStartFameDistribution.svelte
@@ -2,7 +2,11 @@
   import Modal from '$lib/components/Modal.svelte';
   import Icon from '$lib/components/Icon.svelte';
   import Form from '$lib/components/Form.svelte';
-  import { buttonClass, buttonInactiveClass } from '$lib/tokens';
+  import {
+    buttonClass,
+    buttonInactiveClass,
+    buttonColorClass,
+  } from '$lib/tokens';
   import { currentCohort } from '$lib/utils/date';
 
   export let open = false;
@@ -85,6 +89,7 @@
   };
 
   let processing = false;
+  export let agree = false;
 
   // onMount(async () => {
   //   if (!browser) {
@@ -105,45 +110,218 @@
   bind:open
   on:hide={() => handleCancel()}
   ariaLabelledBy="modal-headline"
-  class="max-w-7xl rounded-sm bg-white pl-4 pt-5 pr-8 pb-16 shadow-xl dark:bg-slate-300 sm:p-6"
+  class="max-w-7xl rounded-2xl bg-cyan-900 pl-4 pt-5 pr-8 pb-16 shadow-xl sm:p-6"
 >
-  <div class="mt-3 pt-2 text-center sm:mt-0 sm:ml-4 sm:text-left">
+  <div class="mt-0 ml-4 pt-0 text-left">
     <slot name="header" />
-    <div class="block h-full min-h-[6rem] w-full ">
-      <div>
-        <h4 class="sr-only">Status</h4>
-        <p class="text-sm font-medium text-gray-900">
-          Migrating MySQL database...
-        </p>
-        <div class="mt-6" aria-hidden="true">
-          <div class="overflow-hidden rounded-full bg-gray-200">
-            <div class="h-2 rounded-full bg-cyan-600" style="width: 37.5%" />
+    <div class="mb-6 rounded-2xl bg-cyan-700 py-6">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:mx-0">
+          <h2 class="text-4xl font-bold tracking-tight text-white sm:text-5xl">
+            Give Public Thanks
+          </h2>
+          <p class="mt-4 text-lg leading-6 text-gray-300">
+            This is a great way to recognize those whose work you depend upon.
+            We'll cryptographically sign your kudos and <b>publish them</b> to the
+            global leader board for all to see.
+          </p>
+        </div>
+        <div
+          class="mt-8 flex flex-row items-center justify-between rounded-2xl bg-cyan-600 p-3"
+        >
+          <input
+            type="checkbox"
+            class="h-7 w-7 rounded-full border-0 text-cyan-700 ring-0 focus:ring-0"
+            value="true"
+            bind:checked={agree}
+          />
+          <div class="mx-4 max-w-2xl italic text-cyan-900">
+            I understand that this is a public action and that my kudos will be
+            published with my signature to the global leader board.
           </div>
-          <div
-            class="mt-6 hidden grid-cols-4 text-sm font-medium text-gray-600 sm:grid"
+          <button
+            on:click={() => {
+              if (agree) {
+                // next step handleConfirm();
+              } else {
+                shake = true;
+                setTimeout(() => {
+                  shake = false;
+                }, 500);
+              }
+            }}
+            type="button"
+            class={`cursor-pointer rounded-full border border-cyan-600 bg-gray-300 py-2 px-4 text-sm font-medium text-cyan-900 shadow-sm transition delay-150 ease-in-out hover:bg-gray-200 focus:outline-none focus:ring-0 focus:ring-gray-500 focus:ring-offset-0`}
+            class:animate-shake={shake}
           >
-            <div class="text-cyan-600">Copying files</div>
-            <div class="text-center text-cyan-600">Migrating database</div>
-            <div class="text-center">Compiling assets</div>
-            <div class="text-right">Deployed</div>
-          </div>
+            <div class="flex flex-row">
+              Next
+              {#if processing}
+                <span aria-label={'processing'} class="ml-2 mr-3 animate-spin">
+                  <Icon name="misc/spinner" class="h-5 w-5 text-slate-500" />
+                </span>
+              {:else}
+                <Icon name="chevron-right" class="h-5 w-5 text-cyan-600" />
+              {/if}
+            </div>
+          </button>
         </div>
       </div>
     </div>
+    <nav aria-label="Progress">
+      <ol role="list" class="overflow-hidden">
+        <li class="relative pb-10">
+          <div
+            class="absolute top-4 left-4 -ml-px mt-0.5 h-full w-0.5 bg-cyan-600"
+            aria-hidden="true"
+          />
+          <!-- Complete Step -->
+          <a href="#" class="group relative flex items-center">
+            <span class="flex h-9 items-center">
+              <span
+                class="relative z-10 flex h-8 w-8 items-center justify-center rounded-full bg-cyan-600 group-hover:bg-cyan-800"
+              >
+                <svg
+                  class="h-5 w-5 text-white"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+            </span>
+            <span class="ml-4 flex min-w-0 flex-col">
+              <span class="text-md font-medium text-cyan-50"
+                >Confirm Publishing Plan</span
+              >
+            </span>
+          </a>
+        </li>
+
+        <li class="relative pb-10">
+          <div
+            class="absolute top-4 left-4 -ml-px mt-0.5 h-full w-0.5 bg-gray-300"
+            aria-hidden="true"
+          />
+          <!-- Current Step -->
+          <a
+            href="#"
+            class="group relative flex items-start"
+            aria-current="step"
+          >
+            <span class="flex h-9 items-center" aria-hidden="true">
+              <span
+                class="relative z-10 flex h-8 w-8 items-center justify-center rounded-full border-2 border-cyan-600 bg-white"
+              >
+                <span class="h-2.5 w-2.5 rounded-full bg-cyan-600" />
+              </span>
+            </span>
+            <span class="ml-4 flex min-w-0 flex-col">
+              <span class="text-sm font-medium text-cyan-600"
+                >Profile information</span
+              >
+              <span class="text-sm text-gray-500"
+                >Cursus semper viverra facilisis et et some more.</span
+              >
+            </span>
+          </a>
+        </li>
+
+        <li class="relative pb-10">
+          <div
+            class="absolute top-4 left-4 -ml-px mt-0.5 h-full w-0.5 bg-gray-300"
+            aria-hidden="true"
+          />
+          <!-- Upcoming Step -->
+          <a href="#" class="group relative flex items-start">
+            <span class="flex h-9 items-center" aria-hidden="true">
+              <span
+                class="relative z-10 flex h-8 w-8 items-center justify-center rounded-full border-2 border-gray-300 bg-white group-hover:border-gray-400"
+              >
+                <span
+                  class="h-2.5 w-2.5 rounded-full bg-transparent group-hover:bg-gray-300"
+                />
+              </span>
+            </span>
+            <span class="ml-4 flex min-w-0 flex-col">
+              <span class="text-sm font-medium text-gray-500"
+                >Business information</span
+              >
+              <span class="text-sm text-gray-500">Penatibus eu quis ante.</span>
+            </span>
+          </a>
+        </li>
+
+        <li class="relative pb-10">
+          <div
+            class="absolute top-4 left-4 -ml-px mt-0.5 h-full w-0.5 bg-gray-300"
+            aria-hidden="true"
+          />
+          <!-- Upcoming Step -->
+          <a href="#" class="group relative flex items-start">
+            <span class="flex h-9 items-center" aria-hidden="true">
+              <span
+                class="relative z-10 flex h-8 w-8 items-center justify-center rounded-full border-2 border-gray-300 bg-white group-hover:border-gray-400"
+              >
+                <span
+                  class="h-2.5 w-2.5 rounded-full bg-transparent group-hover:bg-gray-300"
+                />
+              </span>
+            </span>
+            <span class="ml-4 flex min-w-0 flex-col">
+              <span class="text-sm font-medium text-gray-500">Theme</span>
+              <span class="text-sm text-gray-500"
+                >Faucibus nec enim leo et.</span
+              >
+            </span>
+          </a>
+        </li>
+
+        <li class="relative">
+          <!-- Upcoming Step -->
+          <a href="#" class="group relative flex items-start">
+            <span class="flex h-9 items-center" aria-hidden="true">
+              <span
+                class="relative z-10 flex h-8 w-8 items-center justify-center rounded-full border-2 border-gray-300 bg-white group-hover:border-gray-400"
+              >
+                <span
+                  class="h-2.5 w-2.5 rounded-full bg-transparent group-hover:bg-gray-300"
+                />
+              </span>
+            </span>
+            <span class="ml-4 flex min-w-0 flex-col">
+              <span class="text-sm font-medium text-gray-500">Preview</span>
+              <span class="text-sm text-gray-500"
+                >Iusto et officia maiores porro ad non quas.</span
+              >
+            </span>
+          </a>
+        </li>
+      </ol>
+    </nav>
   </div>
-  <div class="mt-4 ml-4 flex flex-row-reverse">
+  <div class="mt-4 ml-4 flex hidden flex-row-reverse">
     <button
       on:click={handleConfirm}
       type="button"
-      class={`${buttonClass}`}
+      class={`${buttonClass} border-0 bg-gray-500 hover:bg-gray-600`}
       class:animate-shake={shake}
     >
-      Ok
-      {#if processing}
-        <span aria-label={'processing'} class="ml-2 mr-3 animate-spin">
-          <Icon name="misc/spinner" class="text-primary-500 h-5 w-5" />
-        </span>
-      {/if}
+      <div class="flex flex-row">
+        Next
+        {#if processing}
+          <span aria-label={'processing'} class="ml-2 mr-3 animate-spin">
+            <Icon name="misc/spinner" class="h-5 w-5 text-slate-500" />
+          </span>
+        {:else}
+          <Icon name="chevron-right" class="h-5 w-5 text-white" />
+        {/if}
+      </div>
     </button>
     {#if cancelActive}
       <button
@@ -151,7 +329,7 @@
           open = false;
         }}
         type="button"
-        class={`${buttonInactiveClass} mr-2`}
+        class={`${buttonInactiveClass} mr-2 border-0`}
       >
         Cancel
       </button>
--- src/lib/tokens/index.ts
+++ src/lib/tokens/index.ts
@@ -3,8 +3,18 @@
 // so you need to use it at least once in the codebase to make sure it's included in the final bundle
 // this is a limitation of the current implementation of the tailwindcss plugin
 
-export const buttonClass =
-  'cursor-pointer rounded-full border border-gray-300 bg-blue-700 py-2 px-4 text-sm font-medium text-white shadow-sm transition delay-150 ease-in-out hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2';
+export const buttonColorClass = ({ color, text, border }) => {
+  color = color || 'gray';
+  text = text || 'white';
+  border = border || 'gray';
+  return `cursor-pointer rounded-full border border-${border}-300 bg-${color}-700 py-2 px-4 text-sm font-medium text-${text} shadow-sm transition delay-150 ease-in-out hover:bg-${color}-700 focus:outline-none focus:ring-2 focus:ring-${color}-500 focus:ring-offset-2`;
+};
+
+export const buttonClass = buttonColorClass({
+  border: 'gray',
+  color: 'blue',
+  text: 'white',
+});
 
 export const buttonInactiveClass =
   'cursor-pointer rounded-full border border-gray-300 bg-gray-700 py-2 px-4 text-sm font-medium text-white shadow-sm transition delay-150 ease-in-out hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2';
