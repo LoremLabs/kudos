57d8b2f9bbe95bffab0f960c793dbb9b98f61677
Matt Mankins <mankins@gmail.com>
2023-07-03 14:45:34 +0200

    fix(setler-cli): 0.0.16

--- identity-wallet/setler-cli/src/lib/wallet/generateMnemonic.js
+++ identity-wallet/setler-cli/src/lib/wallet/generateMnemonic.js
@@ -1,6 +1,6 @@
 import * as bip39 from "@scure/bip39";
 
-import { wordlist } from "@scure/bip39/wordlists/english";
+import { wordlist } from "@scure/bip39/wordlists/english.js";
 
 export const generateMnemonic = (bits = 256) => {
   const mnemonic = bip39.generateMnemonic(wordlist, bits);
