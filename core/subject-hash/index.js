9a31212f7dcadc341bfc8202bbd6e70410549ceb
Matt Mankins <mankins@gmail.com>
2023-08-09 19:41:59 +0200

    feature(subject-hash): ya know, check the right file in.

--- core/subject-hash/index.js
+++ core/subject-hash/index.js
@@ -1,8 +1,64 @@
-import UuidEncoder from 'uuid-encoder';
-import { v1 as uuid } from 'uuid';
+import { sha256 } from "@noble/hashes/sha256";
+import { bytesToHex as toHex } from "@noble/hashes/utils";
+import { Resolver } from "dns/promises";
 
-const encoder = new UuidEncoder('base58');
+const resolver = new Resolver();
 
-export const shortId = () => encoder.encode(uuid());
-export const shortIdFromUuid = (uuid) => encoder.encode(uuid);
-export const uuidV1 = () => uuid();
+export const getSubjectSubdomain = async (subj) => {
+  // calculate the sha256 of the subject
+  const subject = subj.toLowerCase().trim();
+
+  // subject would be email:someone@example.com or whatever identifier you want, with a type prefix
+
+  const subjectHash = toHex(sha256(subject)); // 64 bytes, need to split in two for dns. one other thing to note: we can't have all numbers, so if that's the case we will add a prefix
+
+  // split subjectHash in two equal parts
+  let part1 = subjectHash.substring(0, 32);
+  let part2 = subjectHash.substring(32);
+
+  // check if part1 is all numbers
+  if (/^\d+$/.test(part1)) {
+    // add a prefix
+    part1 = "sha256-" + part1; // prefix should be ignored by the resolver
+  }
+
+  // check if part2 is all numbers
+  if (/^\d+$/.test(part2)) {
+    // add a prefix
+    part2 = "sha256-" + part2;
+  }
+  const subjHash = `${part1}.${part2}`;
+  return subjHash;
+};
+
+// lookup a subject, get their payment address via dns
+export const getSubjectPayVia = async ({
+  subject, // subject is the identifier, like email:foo@bar or twitter:@loremlabs
+  network = "xrpl-mainnet",
+  domain = "ident.cash",
+}) => {
+  const subdomain = await getSubjectSubdomain(subject);
+  const host = `${network.replace(":", "-")}.${subdomain}.${domain}`;
+
+  let txtRecord;
+
+  try {
+    const response = await resolver.resolve(host, "TXT");
+    txtRecord = response[0][0];
+    console.log({ txtRecord });
+    if (txtRecord) {
+      // remove any outer quotes
+      txtRecord = txtRecord.replace(/^"(.*)"$/, "$1");
+    }
+  } catch (e) {
+    if (e.message && !e.message.includes("ENODATA")) {
+      console.log(e);
+    }
+  }
+
+  if (txtRecord) {
+    // TODO: support escrow type responses
+    return { payVia: txtRecord };
+  }
+  return {};
+};
