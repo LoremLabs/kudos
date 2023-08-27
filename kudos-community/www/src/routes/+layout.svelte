caa1a019a1baa88612901e0e4d6ffeaf24a2ad42
Matt Mankins <mankins@gmail.com>
2023-08-27 17:07:57 +0200

    feature[www-kudos-community]: remove unneeded redirect

--- kudos-community/www/src/routes/+layout.svelte
+++ kudos-community/www/src/routes/+layout.svelte
@@ -3,25 +3,10 @@
 	import '$lib/styles/twemoji.scss';
 
 	import Toasts from '$lib/components/Toasts.svelte';
-
-	import { onMount } from 'svelte';
-
-	// see if the query params contained a favicon
-	let faviconItem;
-	let favicon = '/favicon';
-
-	onMount(() => {
-		faviconItem = new URLSearchParams(window.location.search).get('favicon');
-
-		// add leading zeros
-		favicon = faviconItem
-			? `/favicon?favicon=${encodeURIComponent(faviconItem.padStart(1, '0'))}`
-			: '/favicon';
-	});
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicons/favicon-35.png" />
 	<script
 		src="https://cdn.usefathom.com/script.js"
 		data-spa="auto"
